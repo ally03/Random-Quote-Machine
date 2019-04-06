3a256482ca788dabbb50a9b8df11e6ae3ec26581
Alid Didarali <aliddidarali@Alids-MacBook-Pro.local>
2019-04-06 13:33:21 +0530

    display random string from the array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import RandomQuote from "./RandomQuote";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <h1>hi it;s me alid </h1>
-        </header>
+      <div className="App-header">
+        <div id="quote-box" className="App-middle">
+          <RandomQuote />
+          <div>
+            <button id="tweet-quote">tweet-quote</button>
+            <button id="tweet-quotee">facebook-quote</button>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ my-app/src/RandomQuote.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import "./App.css";
+
+const quoteShuffle = [
+  {
+    id: 1,
+    quote:
+      "You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.",
+    author: "-Dr. Suess"
+  },
+  {
+    id: 2,
+    quote:
+      "I’m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can’t handle me at my worst, then you sure as hell don’t deserve me at my best.",
+    author: "-Marilyn Monroe"
+  },
+  {
+    id: 3,
+    quote: "Get busy living or get busy dyin",
+    author: "-Stephen King"
+  },
+  {
+    id: 4,
+    quote:
+      "The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.",
+    author: "-Mark Caine"
+  },
+  {
+    id: 5,
+    quote:
+      "When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us",
+    author: "-Helen Keller"
+  },
+  {
+    id: 6,
+    quote:
+      "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do",
+    author: "-Mark Twain"
+  }
+];
+
+class RamdomQuote extends Component {
+  // constructor([props]) {
+  //     super(props);
+  //     this.state ={
+  //         nextQuote: ''
+  //     };
+  render() {
+    console.log(quoteShuffle);
+    const renObjData =
+      quoteShuffle[Math.floor(Math.random() * quoteShuffle.length)];
+    // const renObjData = quoteShuffle.map(function(quoteShuffle, ida, idx) {
+    //   return [
+    //     <p key={ida}>{quoteShuffle.quote}</p>,
+    //     <p key={idx}>{quoteShuffle.author}</p>
+    //   ];
+    // });
+    console.log(renObjData);
+    return (
+      <div>
+        <h1>{renObjData.quote}</h1>
+        <p id="author" className="author">
+          {renObjData.author}
+        </p>
+        <button className="App-newQuote" id="new-quote">
+          new quote
+        </button>
+      </div>
+    );
+  }
+}
+
+export default RamdomQuote;
